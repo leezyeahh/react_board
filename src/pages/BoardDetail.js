dbee133356d02f4fb68dde0fdd35c48a58427b06
leezyeahh <leezyeahh@gmail.com>
2025-11-07 18:58:16 +0900

    - 뒤로가기 버튼 추가

--- src/pages/BoardDetail.js
+++ src/pages/BoardDetail.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useParams  } from 'react-router-dom';
 import { getPostById } from '../api/localStorage';
+import { useNavigate } from "react-router-dom";
+import Button from "../components/atoms/Button";
 
 const BoardDetail = () => {
     const { id } = useParams();
     const [ post, setPost ] = useState(null);
+  const navigate = useNavigate();
+    const goBack = () => {
+      navigate(-1);
+    };
 
     useEffect(() => {
         const detailPost = getPostById(id);
@@ -25,6 +31,7 @@ const BoardDetail = () => {
             <h2 className="ptitle">게시글 상세</h2>
             <p>{post.title}</p>
             <div>{post.content}</div>
+          <Button size='sm' color='black' onClick={goBack}><span className="text">뒤로</span></Button>
         </div>
     )
 }
